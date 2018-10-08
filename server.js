ce15956966ac1cae5b7392aef93676ebaadc57fd
Kamakshidasan <adhitya.kamakshidasan@gmail.com>
2018-10-08 09:34:14 +0200

    webm example

--- server.js
+++ server.js
@@ -1,11 +1,3 @@
-/**
- *  Copyright (c) 2017-present, Facebook, Inc.
- *  All rights reserved.
- *
- *  This source code is licensed under the license found in the
- *  LICENSE file in the root directory of this source tree.
- */
-
 const child_process = require('child_process');
 const express = require('express');
 const WebSocketServer = require('ws').Server;
@@ -28,63 +20,70 @@ app.use((req, res, next) => {
 app.use(express.static(__dirname + '/www'));
 
 wss.on('connection', (ws) => {
-  
+
   // Ensure that the URL starts with '/rtmp/', and extract the target RTMP URL.
   let match;
   if ( !(match = ws.upgradeReq.url.match(/^\/rtmp\/(.*)$/)) ) {
     ws.terminate(); // No match, reject the connection.
     return;
   }
-  
-  const rtmpUrl = decodeURIComponent(match[1]);
+
+  //const rtmpUrl = decodeURIComponent(match[1]);
+  const rtmpUrl = __dirname + '/www' + "/test.webm";
+
   console.log('Target RTMP URL:', rtmpUrl);
-  
+
   // Launch FFmpeg to handle all appropriate transcoding, muxing, and RTMP
   const ffmpeg = child_process.spawn('ffmpeg', [
+    // if file exists delete it
+    '-y',
+
     // Facebook requires an audio track, so we create a silent one here.
     // Remove this line, as well as `-shortest`, if you send audio from the browser.
-    '-f', 'lavfi', '-i', 'anullsrc',
-    
+    '-f', 'lavfi',
+
+    //'-i', 'anullsrc',
+
     // FFmpeg will read input video from STDIN
     '-i', '-',
-    
+
     // Because we're using a generated audio source which never ends,
     // specify that we'll stop at end of other input.  Remove this line if you
     // send audio from the browser.
     '-shortest',
-    
+
     // If we're encoding H.264 in-browser, we can set the video codec to 'copy'
     // so that we don't waste any CPU and quality with unnecessary transcoding.
     // If the browser doesn't support H.264, set the video codec to 'libx264'
     // or similar to transcode it to H.264 here on the server.
-    '-vcodec', 'copy',
-    
+    '-vcodec', 'libvpx-vp9',
+
     // AAC audio is required for Facebook Live.  No browser currently supports
     // encoding AAC, so we must transcode the audio to AAC here on the server.
-    '-acodec', 'aac',
-    
+    '-acodec', 'libvorbis',
+
     // FLV is the container format used in conjunction with RTMP
-    '-f', 'flv',
-    
+    '-f', 'webm',
+
     // The output RTMP URL.
     // For debugging, you could set this to a filename like 'test.flv', and play
     // the resulting file with VLC.
-    rtmpUrl 
+    rtmpUrl
   ]);
-  
+
   // If FFmpeg stops for any reason, close the WebSocket connection.
   ffmpeg.on('close', (code, signal) => {
     console.log('FFmpeg child process closed, code ' + code + ', signal ' + signal);
     ws.terminate();
   });
-  
+
   // Handle STDIN pipe errors by logging to the console.
   // These errors most commonly occur when FFmpeg closes and there is still
   // data to write.  If left unhandled, the server will crash.
   ffmpeg.stdin.on('error', (e) => {
     console.log('FFmpeg STDIN Error', e);
   });
-  
+
   // FFmpeg outputs all of its messages to STDERR.  Let's log them to the console.
   ffmpeg.stderr.on('data', (data) => {
     console.log('FFmpeg STDERR:', data.toString());
@@ -95,10 +94,10 @@ wss.on('connection', (ws) => {
     console.log('DATA', msg);
     ffmpeg.stdin.write(msg);
   });
-  
+
   // If the client disconnects, stop FFmpeg.
   ws.on('close', (e) => {
     ffmpeg.kill('SIGINT');
   });
-  
+
 });
